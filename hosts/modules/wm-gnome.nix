1bf5f814628579eea065bee558896fa7502c400d
bombonato <fabio.bombonato@gmail.com>
2025-06-30 12:17:06 -0300

    (feat) Gnome Light - exclude some aditional or not required packages from gnome desktop

--- hosts/modules/wm-gnome.nix
+++ hosts/modules/wm-gnome.nix
@@ -24,4 +24,26 @@
   services.gnome.gnome-remote-desktop.enable = true;
 
   systemd.services."gnome-remote-desktop".wantedBy = [ "graphical.target" ];
+
+  environment.gnome.excludePackages = with pkgs; [
+    gnome-bluetooth
+    cheese       # webcam
+    epiphany     # web browser
+    #gedit       # text editor
+    #eog         # image viewer
+    totem        # video player
+    yelp         # help viewer
+    geary        # email client
+    evince       # document viewer
+    simple-scan  # document scanner
+    gnome-calendar     # calendar
+    gnome-contacts     # contacts
+    gnome-maps      # maps
+    gnome-music     # music
+    gnome-photos     # photos
+    gnome-tour       # tour app
+    gnome-font-viewer #no need
+    gnome-user-docs
+    gnome-weather #weather app
+  ];
 }
